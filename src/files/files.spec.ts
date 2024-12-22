80c37ec26a515860b70671b46edeb784ede8d458
ILIYA <99527242+KARMA-143@users.noreply.github.com>
2024-12-22 18:00:42 +0300

    add server files

---
+++ src/files/files.spec.ts
@@ -0,0 +1,7 @@
+import { Files } from './files';
+
+describe('Files', () => {
+  it('should be defined', () => {
+    expect(new Files()).toBeDefined();
+  });
+});
---
+++ src/lessons/lessons.spec.ts
@@ -0,0 +1,7 @@
+import { Lessons } from './lessons';
+
+describe('Lessons', () => {
+  it('should be defined', () => {
+    expect(new Lessons()).toBeDefined();
+  });
+});
